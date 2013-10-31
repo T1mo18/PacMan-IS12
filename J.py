9dfac257fbd191f6232a1794ee409b74b2a24114
Jurlak <jasper.kirotosk@gmail.com>
2013-10-31 09:40:52 +0200

    uuendus

--- J.py
+++ J.py
@@ -1,22 +1,24 @@
-!/usr/bin/env python
- -*- coding: utf-8 -*-
+!/usr/bin/env python 
+ -*- coding utf-8 -*-
 
 from msvcrt import getch
-while True:
-    key = ord(getch())
-    if key == 
-        break
-    elif key == 
-        select()
-    elif key == 
-		break
-    if key == 
-			
-def player_print("@"):
-		print
+
+def player_print():
+	while True:
+		key = ord(getch())
+		if key == 65
+			break
+		elif key == 68
+			select()
+		elif key == 83
+			break
+		if key == 87
+
+player_print()			
+		
 def player_next():
 
-
+#A=65 D=68 S=83 W=87
 
 
 
