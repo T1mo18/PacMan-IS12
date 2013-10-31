4f3488f2f85f2c401f0fab40bce1e0717390b954
opilane <opilane@PC215-10.(none)>
2013-10-31 08:47:35 +0200

    Tervist

---
+++ main.py
@@ -0,0 +1,14 @@
+
+def menu()
+	kaart_print()
+	elukas_print()
+	player_print()
+	elukas_next()
+	player_next()
+	
+while True:
+	kaart_print()
+	elukas_print()
+	player_print()
+	elukas_next()
+	player_next()
