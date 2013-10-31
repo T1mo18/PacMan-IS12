109a2a4a589ce9f00c83eb5b0fddd8e728112ac6
Jurlak <jasper.kirotosk@gmail.com>
2013-10-31 08:41:52 +0200

    j

---
+++ J.py
@@ -0,0 +1,28 @@
+!/usr/bin/env python
+ -*- coding: utf-8 -*-
+
+from msvcrt import getch
+while True:
+    key = ord(getch())
+    if key == 
+        break
+    elif key == 
+        select()
+    elif key == 
+		break
+    if key == 
+			
+def player_print("@"):
+		print
+def player_next():
+
+
+
+
+
+
+
+
+
+def player_xy():
+	pass
