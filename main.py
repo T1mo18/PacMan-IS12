9381d295815b7e910ac4e59c2054ec6eb9ec66d1
Timo <teder.timo@gmail.com>
2013-10-31 09:28:19 +0200

    Toimiv main

--- main.py
+++ main.py
@@ -1,17 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 def main():		
-
-	kaart_print()
-	elukas_print()
-	player_print()
-	elukas_next()
-	player_next()
-
-	
-while True:
-	kaart_print()
-	elukas_print()
-	player_print()
-	elukas_next()
-	player_next()
+		while True:
+			kaart_print()
+			elukas_print()
+			player_print()
+			elukas_next()
+			player_next()
+		
+main()
