7b8a552f6586166c33d11499f0510cccb942761d
Timo <teder.timo@gmail.com>
2013-11-06 15:47:46 +0200

    Create mainb.py

---
+++ mainb.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python 
+#-*- coding utf-8 -*-
+
+from kaart import kaart_print
+from elukad import elukas_print
+from mangija import player_print
+
+
+
+
+
+
+def main():                
+                while True:
+                        kaart_print()
+                        elukas_print()
+                        player_print()
+                        elukas_next()
+                        player_next()
+                
+main()
