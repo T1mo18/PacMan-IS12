eea103d1c396c7196d3e3cb7e246bcdc9fbb09be
kerto <kerto.kangur@hotmail.com>
2013-11-06 15:05:40 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+from kaart import kaart_print
+from player import player_print
+from elukad import elukas_print
+
 def main():		
 		while True:
 			kaart_print()
