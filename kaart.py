57d7253ca939b1afc1970cbe5fd4a87a9346065f
Aigar Vetemäe <aigar@aigar-HP-Pavilion-g6-Notebook-PC.(none)>
2013-11-06 15:56:06 +0200

    ekraan puhtaks

--- kaart.py
+++ kaart.py
@@ -1,3 +1,4 @@
+import sys
 kaart= [
 "#######################",
 "#**********#**********#",
@@ -26,11 +27,9 @@ kaart= [
 ]
 
 def kaart_print():
+	sys.stdout.write("\033[1;1H")
 	for rida in kaart:
 		print rida
 
-
-def kaart_xy(x, y):
-	
-	print kaart [y-1] [x-1]
-
+def kaart_xy(x,y):
+	print kaart[y-1] [x-1]
