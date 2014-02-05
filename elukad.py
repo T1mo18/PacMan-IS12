de219e615203156e53104df617f1526904b297b1
Kairo Kübard <kairo.kybard@gmail.com>
2014-02-05 09:28:29 +0200

    Update elukad.py

--- elukad.py
+++ elukad.py
@@ -11,25 +11,15 @@ def elukas_print():
 
 	print elukas
 
-def elukas_next():
-	# Get pacman position
-	pacman = player_xy()
-	
-	px = pacman[0]
-	py = pacman[1]
+def elukas_move(direction):
 
-	# Search pacman from right
-	if px >= x and py >= y:
+	if direction == 'right':
+
 		# Look right
 		if kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
 			suund = 'right'
-		# Look left
-		elif kaart_xy(x-1, y) == 1:
-			x = x-1
-			y = y
-			suund = 'left'
 		# Look up
 		elif kaart_xy(x, y-1) == 1:
 			x = x
@@ -39,79 +29,108 @@ def elukas_next():
 		elif kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
-			suund = 'down'
-			
+			suund = 'down'	
+		# Look left
+		elif kaart_xy(x-1, y) == 1:
+			x = x-1
+			y = y
+			suund = 'left'	
 
-	# Search pacman from left
-	if px <= x and py == y:
+	if direction == 'left':
+
 		# Look left
 		if kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
-			suund = 'left'
+			suund = 'left'	
+		# Look up
+		elif kaart_xy(x, y-1) == 1:
+			x = x
+			y = y-1
+			suund = 'up'
+		# Look down
+		elif kaart_xy(x, y+1) == 1:
+			x = x
+			y = y+1
+			suund = 'down'	
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
 			suund = 'right'
-		# Look up
-		elif kaart_xy(x, y-1) == 1:
-			x = x
-			y = y-1
-			suund = 'up'
-		# Look down
-		elif kaart_xy(x, y+1) == 1:
-			x = x
-			y = y+1
-			suund = 'down'
-			
 
-	# Search pacman from top
-	if px == x and py <= y:
+	if direction == 'up':
+
 		# Look up
 		if kaart_xy(x, y-1) == 1:
 			x = x
 			y = y-1
 			suund = 'up'
-		# Look down
-		elif kaart_xy(x, y+1) == 1:
-			x = x
-			y = y+1
-			suund = 'down'
 		# Look left
 		elif kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
-			suund = 'left'
+			suund = 'left'	
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
 			suund = 'right'
+		# Look down
+		elif kaart_xy(x, y+1) == 1:
+			x = x
+			y = y+1
+			suund = 'down'	
 
-	# Search pacman from bottom
-	if px == x and py >= y:
+	if direction == 'down':
+
 		# Look down
 		if kaart_xy(x, y+1) == 1:
 			x = x
 			y = y+1
 			suund = 'down'
-		# Look up
-		elif kaart_xy(x, y-1) == 1:
-			x = x
-			y = y-1
-			suund = 'up'
 		# Look left
 		elif kaart_xy(x-1, y) == 1:
 			x = x-1
 			y = y
-			suund = 'left'
+			suund = 'left'	
 		# Look right
 		elif kaart_xy(x+1, y) == 1:
 			x = x+1
 			y = y
 			suund = 'right'
-				
+		# Look up
+		elif kaart_xy(x, y-1) == 1:
+			x = x
+			y = y-1
+			suund = 'up'
+
+
+def elukas_next():
+	# Get pacman position
+	pacman = player_xy()
+
+	px = pacman[0]
+	py = pacman[1]
+
+	# Search pacman from right
+	if px >= x and py >= y:
+		elukas_move('right');
+
+
+	# Search pacman from left
+	if px <= x and py == y:
+		elukas_move('left');
+
+
+	# Search pacman from top
+	if px == x and py <= y:
+		elukas_move('top');
+
+	# Search pacman from bottom
+	if px == x and py >= y:
+		elukas_move('bottom');
+
 
 
 def elukas_xy():
