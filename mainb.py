cf4629e65c35a7d84c72b95306a943c2e80cc294
Timo <teder.timo@gmail.com>
2014-02-06 14:49:45 +0200

    Update mainb.py

--- mainb.py
+++ mainb.py
@@ -3,19 +3,18 @@
 
 from kaart import kaart_print
 from elukad import elukas_print
-from mangija import player_print
-
+from player import player_print
 
 
 
 
 
 def main():                
-                while True:
-                        kaart_print()
-                        elukas_print()
-                        player_print()
-                        elukas_next()
-                        player_next()
+	while True:
+		kaart_print()
+		elukas_print()
+		player_print()
+		#elukas_next()
+		#player_next()
                 
 main()
