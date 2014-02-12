352be186707254e5e5049bfff6ea8889cd563a33
Timo <teder.timo@gmail.com>
2014-02-12 08:59:20 +0200

    Update mainb.py

--- mainb.py
+++ mainb.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python 
 #-*- coding utf-8 -*-
-
 from kaart import kaart_print
 from elukad import elukas_print
+from elukad import elukas_next
+from elukad import elukas_xy
 from player import player_print
+from player import player_xy
+from player import player_next
 import time
-
-
 
 
 
@@ -15,7 +16,9 @@ def main():
 		kaart_print()
 		elukas_print()
 		player_print()
-		#elukas_next()
-		#player_next()
-      		time.sleep(1)
+		elukas_next()
+		player_next()
+		time.sleep(0.3)
+		if elukas_xy()== player_xy():
+			print "G4M3 0V3R"	
 main()
