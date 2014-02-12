c3503d20d79710d2af697ececd1f5c0404144785
aulis <aulis.s09@gmail.com>
2014-02-12 09:39:37 +0200

    yolo

--- player.py
+++ player.py
@@ -1,14 +1,38 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
+from elukad import *
+from kaart import *
+
+x = 10 
+y = 10
 
 def player_print():
-	pass
-		
+	player = "\033["+str(y)+";"+str(x)+"H" + "G"
+	player = "\033[93m" + player + "\033[0m"
+
+	print player
 
 def player_next():
-	pass		 
+	# 'j' = left
+	# 'l' = right
+	# 'i' = up
+	# 'k' = down
+	global x
+	global y
 	
-
-
+	while True:
+		mov = raw_input("Sisesta liikumissuund (j,l,i,k) -> ")
+		if mov == "i":
+			y = y + 1
+			print player_next
+		elif mov == "j":
+			x = x - 1
+			print player
+		elif mov == "l":
+			x = x + 1
+			print player
+		elif mov == "k":
+			y = y - 1
+			print player
+		
 def player_xy():
-	pass	
+    global x
+    global y
