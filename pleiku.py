c0a4a7d4f8c4fbb28260f9e9ab32ae49463cae5d
opilane <opilane@PC215-10.(none)>
2014-02-12 08:50:51 +0200

    yolo

---
+++ pleiku.py
@@ -0,0 +1,17 @@
+from kaart import *
+from elukas import *
+from elukas import *
+
+x = 10 
+y = 10
+def player_print(x,y,tekst):
+	player = "\033["+str(y)+";"+str(x)+"H" + "G"
+	player = "\033[93m" + player + "\033[0m"
+	
+	print player
+	
+def player_next():
+	pass
+
+def player_xy():
+	pass
